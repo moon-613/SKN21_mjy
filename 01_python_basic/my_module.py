59ecd019280eadba9ec903ced2fe6cf0764edf88
moon-613 <mjy6812@naver.com>
2025-11-19 18:09:40 +0900

    first commit

---
+++ 01_python_basic/my_module.py
@@ -0,0 +1,24 @@
+# 변수
+__version__ = 0.1  # 밑에 이렇게 하면 전역변수.
+
+def plus(num1, num2):
+    return num1 + num2
+
+def minus(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    return num1 / num2
+
+# 실행구문.
+# 위에 정의한 함수들 호출.
+#result = plus (10, 20)
+#print(result)
+#result = minus(result, 50)
+#print(result)
+
+# python  my_module.py (script 파일의 경로)
+# control + j (터미널 실행)
