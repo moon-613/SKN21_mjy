82611bb6e99cb3742f79b038db35c99dfdc619ea
moon-613 <mjy6812@naver.com>
2026-01-16 15:30:09 +0900

    href

--- 13_django/mypoll/polls/urls.py
+++ 13_django/mypoll/polls/urls.py
@@ -4,3 +4,16 @@
 # 매핑 설정 - path("url경로", View, name="설정 식별 이름") 함수 이용
 
 from django.urls import path
+from . import views  # 상대 경로로 import . : 현재 모듈이 있는 패키지
+
+app_name = "polls"  # 전체 설정에 대한 prefix(namespace). 설정 name에 공통적으로 앞에 붙일 이름 ("app_name:name")
+
+urlpatterns = [
+    path("welcome", views.welcome, name="welcome"),
+    path("list", views.list, name="list"),
+    path("vote_form/<int:question_id>", views.vote_form, name="vote_form"),
+]
+# polls/vote_form/<path 파라미터값 타입:view의 파라미터 변수명>
+# <int:     # path parameter값을 int로 전달해라.
+# <   : question_id>  # view의 question_id 변수로 전달해라. 
+# type: int, str
--- 13_django/mypoll/polls/views.py
+++ 13_django/mypoll/polls/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from datetime import datetime
+from .models import Question, Choice
 
 # Create your views here.
 
@@ -10,8 +11,22 @@ def welcome (request):  # 최소 한 개 파라미터는 선언해야 함
     print("Welcome 실행")
     # 요청 처리
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    # 응답 화면 생성 -> template 호출 (template이 사용할 값을 전달)
+    response = render(  # template 호출 -결과-> HttpResponse로 반환
+        request,  # HttpRequest
+        "polls/welcome.html",  # 호출할 template의 경로 
+        {"now":now}  # template에 전달할 값들. name-value 로 전달. Context Value 라고 한다. 
+    )
+    print(type(response))  # server를 실행한 터미널에 출력
+    return response
+
+
+def welcome_old(request):
+    print("welcome 실행")
+    # 요청 처리
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     # 처리 결과 페이지 생성 -> html을 str로 구현
-    res_html = f"""<!DOCTYPE>
+    res_html = f"""<!DOCTYPE HTML>
 <html>
     <head>
         <title>설문</title>
@@ -25,3 +40,51 @@ def welcome (request):  # 최소 한 개 파라미터는 선언해야 함
 """
     res = HttpResponse(res_html)
     return res
+
+
+# 설문 (질문) 목록 조회 
+## 전체 question들을 조회해 목록 html을 반환 
+## 요청 url: polls/list
+## view함수: list
+## template: polls./list.html 
+def list(request):
+    # 1. DB에서 질문 목록 조회 conn -> cursor -> sql 실행 -> Model 사용 
+    question_list = Question.objects.all().order_by("-pub_date")
+    # 2. 응답 페이지 생성(template 사용) -> 반환
+    return render(
+        request, 
+        "polls/list.html", 
+        {"question_list":question_list}      
+    )
+
+
+# 개별 설문을 할 수 있는 페이지(설문 폼)로 이동 
+## 질문 ID를 path parameter로 받아서
+##     그 질문과 보기를 DB에서 조회해서 화면에 출력 - 설문 입력 폼
+### path parameter: http://ip:port/path1/path2/path3/전달값1/전달값2
+### request(요청) parameter: http://ip:port/path1/path2/path3?=name=전달값1&name=전달값2
+
+# 요청 URL: /polls/vote_form/질문ID
+# View함수: vote_form
+# template: polls/vote_form.html
+def vote_form(request, question_id):
+    # question_id 파라미터 - path parameter 값을 받을 변수
+    # view함수의 두 번째 파라미터부터는 path parameter를 받을 변수들
+    ## 파라미터 변수명은 urls.py에 등록한 변수명으로 선언하면 된다.
+
+    # 1. DB에서 question_id로 질문을 조회
+    try:
+        question = Question.objects.get(pk=question_id)
+        # 응답 화면 요청
+        return render(
+            request, 
+            "polls/vote_form.html",
+            {"question":question}
+        )
+    except:
+        print(f"{question_id}의 질문이 없습니다.")
+
+
+# 설문 하기  
+
+
